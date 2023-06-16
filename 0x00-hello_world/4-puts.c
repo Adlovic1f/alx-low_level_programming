79a8df2c6ffbfae14e6ca11c825d11a836f93621
Adlovic1f <Adil.brave1@gmail.com>
2023-06-15 19:20:13 -0700

    update

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main - function return to void
+*
+* Return: 0 on successful execution
+*/
+int main(void)
+{
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
+}
