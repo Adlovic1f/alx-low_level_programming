3bea19245e9b91637480de2de8ae349516197a75
Adlovic1f <Adil.brave1@gmail.com>
2023-06-15 19:35:50 -0700

    update

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - main - The entry point of the program.
+ *
+ * Return: 0 on successful execution
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
