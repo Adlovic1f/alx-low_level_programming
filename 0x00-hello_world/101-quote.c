b96da3f1d4e18a30d958e44034d01bdcedd2f60f
Adlovic1f <Adil.brave1@gmail.com>
2023-06-15 19:46:24 -0700

    update

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+  * main - The entry point of the program.
+  *
+  * Return: 0 on successful execution
+  */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
