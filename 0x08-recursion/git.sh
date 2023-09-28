c2e25ad2d7f8cc3932f075b0285dcc3969b66484
Adlovic1f <Adil.brave1@gmail.com>
2023-09-28 14:18:57 -0700

    update

---
+++ 0x08-recursion/git.sh
@@ -0,0 +1,53 @@
+#!/bin/bash 
+
+git add 0-main.c
+git commit -m "update"
+git push 
+
+git add 101-wildcmp.c
+git commit -m "update"
+git push
+
+git add 1-print_rev_recursion.c
+git commit -m "update"
+git push
+
+git add 4-pow_recursion.c
+git commit -m "update"
+git push
+
+git add main.h
+git commit -m "update"
+git push
+
+git add 2-strlen_recursion.c
+git commit -m "update"
+git push
+
+git add _putchar.c
+git commit -m "update"
+git push
+
+git add 100-is_palindrome.c
+git commit -m "update"
+git push
+
+git add 6-is_prime_number.c
+git commit -m "update"
+git push
+
+git add 0-puts_recursion.c
+git commit -m "update"
+git push
+
+git add README.md
+git commit -m "update"
+git push
+
+git add 5-sqrt_recursion.c
+git commit -m "update"
+git push
+
+git add 3-factorial.c
+git commit -m "update"
+git push
