322d57a6bcb8f1a42631e66c7ca5a3d11371275e
Adlovic1f <Adil.brave1@gmail.com>
2023-10-27 13:35:48 -0700

    update

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * get_bit - A function that return the value of a bit at a index
+ * @n: The number to get bit from
+ * @index: The index where the bit get at
+ *
+ * Return: The value of the bit or -1 if an error occured
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+
+	if ((n & (1 << index)) == 0)
+		return (0);
+
+	return (1);
+}
---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * set_bit - A function that sets the value of a bit to 1.
+ * @n: The number to set bit in
+ * @index: The index to set bit at
+ *
+ * Return: 1 if it worked, or -1 on error
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+
+	*n ^= (1 << index);
+
+	return (1);
+}
